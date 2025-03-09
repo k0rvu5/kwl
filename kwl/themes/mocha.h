730d53c662d6ef0cf703e6a3a4e700c02ff38aa2
k0rvu5 <safra.seied@outlook.com>
2025-03-09 12:22:36 +0330

    init

---
+++ kwl/themes/mocha.h
@@ -0,0 +1,6 @@
+static uint32_t colors[][3]                = {
+	/*               fg          bg          border    */
+	[SchemeNorm] = { 0xcdd6f4ff, 0x1e1e2eff, 0x282a36ff },
+	[SchemeSel]  = { 0x1e1e2eff, 0xf5c2e7ff, 0xf5c2e7ff },
+	[SchemeUrg]  = { 0,          0,          0x770000ff },
+};
