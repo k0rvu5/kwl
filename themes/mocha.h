6198c867292e1fe0ed55a642eaabe56b50a7556f
k0rvu5 <safra.seied@outlook.com>
2025-03-11 02:03:50 +0330

    added XF86 support

--- themes/mocha.h
+++ themes/mocha.h
@@ -1,6 +1,9 @@
+#define PINK 0xf5c2e7ff
+#define BLUE 0x89b4faff
+
 static uint32_t colors[][3]                = {
 	/*               fg          bg          border    */
 	[SchemeNorm] = { 0xcdd6f4ff, 0x1e1e2eff, 0x282a36ff },
-	[SchemeSel]  = { 0x1e1e2eff, 0xf5c2e7ff, 0xf5c2e7ff },
+	[SchemeSel]  = { 0x1e1e2eff, PINK,       PINK       },
 	[SchemeUrg]  = { 0,          0,          0x770000ff },
 };
